589f11ade8f4aaa973aa0efa8bc789a1b49e0922
l-merta <lukas.merta26@gmail.com>
2024-10-23 00:03:18 +0200

    steam design update
    design nav menu upgrade

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-switchIframe(1);
+const navButtons = document.querySelectorAll("nav button");
 
-function switchIframe(name) {
+switchIframe('design_steam', navButtons[0]); // Starting design
+function switchIframe(name, button) {
   document.querySelector('iframe').src = name;
+  setActiveButton(button);
+}
+function setActiveButton(button) {
+  navButtons.forEach(b => {
+    b.classList.remove("button-active");
+  });
+  button.classList.add("button-active");
 }
