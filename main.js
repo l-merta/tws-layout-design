2f6a730c5388623266432194e98df2c109907cc9
l-merta <lukas.merta26@gmail.com>
2024-10-26 22:30:02 +0200

    Document title + favicon

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 const navButtons = document.querySelectorAll("nav button");
 
-switchIframe('design_fortnite', navButtons[1]); // Starting design
-function switchIframe(name, button) {
-  document.querySelector('iframe').src = name;
-  setActiveButton(button);
+const pages = [
+  {
+    name: "Default",
+    src: "_design"
+  },
+  {
+    name: "Steam",
+    src: "design_steam"
+  },
+  {
+    name: "Fortnite",
+    src: "design_fortnite"
+  },
+]
+
+switchIframe(1); // Starting design
+function switchIframe(index) {
+  document.title = pages[index].name + " - design";
+  setFavicon("images/" + pages[index].name + "_icon_logo.png");
+  console.log("images/" + pages[index].name + "_icon_logo.png");
+  document.querySelector('iframe').src = pages[index].src;
+  setActiveButton(navButtons[index-1]);
 }
 function setActiveButton(button) {
   navButtons.forEach(b => {
     b.classList.remove("button-active");
   });
   button.classList.add("button-active");
+}
+function setFavicon(src) {
+  var link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+      link = document.createElement('link');
+      link.rel = 'icon';
+      document.head.appendChild(link);
+  }
+  link.href = src;
 }
